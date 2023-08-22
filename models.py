fcf9d65cbe21e3ae3d863239b26193ca6a152ef0
Voe <mr.subiter@mail.ru>
2023-08-22 13:50:46 +0500

    Add primary models

---
+++ models.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+from database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    login = Column(String, unique=True)
+    password = Column(String)
+
+    plants = relationship("Plant", back_populates="owner")
+
+class Plant(Base):
+    __tablename__ = "plants"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String)
+    image = Column(String)
+    privacy = Column(Boolean, default=True)
+
+    owner = relationship("User", back_populates="plants")
+
+
+
