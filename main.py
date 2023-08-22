8a23bb00033061fbf7c7e2b1eced95349d4820a8
Voe <mr.subiter@mail.ru>
2023-08-22 17:47:35 +0500

    Init main

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+
+from . import crud, models, schemas
+from database import SessionLocal, engine
+
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User mot found!")
+    return db_user
+
