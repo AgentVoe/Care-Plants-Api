7b8a22966da8e91450c0d0e94ddf391c8164ab51
Voe <mr.subiter@mail.ru>
2023-08-24 17:24:14 +0500

    Add new models 'Watering' and two connecting tables

--- models.py
+++ models.py
@@ -1,6 +1,22 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Table, DateTime, UniqueConstraint
 from sqlalchemy.orm import relationship
 from database import Base
+
+# Many-to-many table to connect Users and Plants
+user_plant = Table('user_plant', Base.metadata,
+                   Column('id', Integer, primary_key=True, index=True),
+                   Column('user_id', ForeignKey('users.id'), primary_key=True),
+                   Column('plant_id', ForeignKey('plants.id'), primary_key=True),
+                   )
+
+# Many-to-many table to connect Plants and Watering
+plant_watering = Table('plant_watering', Base.metadata,
+                       Column('id', Integer, primary_key=True, index=True),
+                       Column('plant_id', ForeignKey('plants.id'), primary_key=True),
+                       Column('watering_id', ForeignKey('watering.id'), primary_key=True)
+                       )
+
+# User, Plant, Watering classes representing tables structure in database
 
 
 class User(Base):
@@ -11,7 +27,8 @@ class User(Base):
     login = Column(String, unique=True)
     password = Column(String)
 
-    plants = relationship("Plant", back_populates="user")
+    plants = relationship("Plant", secondary='user_plant', back_populates="users")
+
 
 class Plant(Base):
     __tablename__ = "plants"
@@ -21,9 +38,19 @@ class Plant(Base):
     description = Column(String)
     image = Column(String)
     privacy = Column(Boolean, default=True)
-    user_id = Column(Integer, ForeignKey("user.id"))
 
-    user = relationship("User", back_populates="plants")
+    users = relationship("User",  secondary='user_plant',  back_populates="plants")
+
+    waterings = relationship("Watering", secondary="plant_watering", back_populates='plants')
 
 
+class Watering(Base):
+    __tablename__ = 'watering'
+
+    id = Column(Integer, primary_key=True, index=True)
+    week_day = Column(Integer)
+    time = Column(DateTime)
+    last_watering = Column(DateTime)
+
+    plants = relationship("Plant", secondary='plant_watering', back_populates='watering')
 
