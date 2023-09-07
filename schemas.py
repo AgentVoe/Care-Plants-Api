90b96992c21bd86d03d7a8e570939cd62fe2f3a3
Voe <mr.subiter@mail.ru>
2023-09-07 09:03:28 +0500

    Add new schemas

--- schemas.py
+++ schemas.py
@@ -10,15 +10,26 @@ class PlantBase(BaseModel):
     privacy: bool
 
 
-class PlantCreate(PlantBase):
-    pass
-
-
 class Plant(PlantBase):
     id: int
 
     class Config:
         from_attributes = True
+
+
+class PlantCreate(PlantBase):
+    pass
+
+
+class PlantUpdate(PlantBase):
+    pass
+    # class Config:
+    #     schema_extra = {
+    #         "title": "title",
+    #         "description": "description",
+    #         "image": "image",
+    #         "privacy": "privacy"
+    #     }
 
 
 class UserBase(BaseModel):
@@ -47,8 +58,8 @@ class UsersSchema(UserBase):
 
 class WateringBase(BaseModel):
     week_day: int
-    time: datetime
-    last_watering: datetime
+    time: datetime.date
+    last_watering: datetime.date
 
 
 class Watering(WateringBase):
@@ -60,3 +71,15 @@ class Watering(WateringBase):
 
 class WateringSchema(WateringBase):
     plants: list[PlantBase]
+
+
+class UserPlantBase(BaseModel):
+    user_id: int
+    plant_id: int
+
+
+class UserPlant(UserPlantBase):
+    id: int
+
+    class Config:
+        from_attributes = True
